5e01155618e35e6172ac2c086b4250f7a953e32f
Averyyyyyy <avery.bowman@ucas-edu.net>
2025-03-24 10:16:15 -0600

    notes not finished

---
+++ notes tracing.py/main.py
@@ -0,0 +1,19 @@
+#Avery bowman
+
+#What is tracing?
+#python -m trace --trace C:\Users\avery.bowman\.vscode\CP2-Projects\notes tracing.py
+
+#What are some ways we can debug by tracing?
+#How do you access the debugger in vs code?
+#What is testing?
+#What are boundary conditions?
+#How do you handle when users give strange inputs?
+
+def add(num_one, num_two):
+    return num_one + num_two
+
+def sub(num_one, num_two):
+    return num_one - num_two
+
+print(add(5,4))
+print(sub(5,4))
